92886eb138b148fd75e9eafcf2c7ac021ef2f8a5
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-10 21:44:01 -0700

    test product and tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -13,10 +13,7 @@ router.get('/', (req, res) => {
       {
         model: Category,
         attributes: ['id', 'category_name'],
-        include: {
-          model: User,
-          attributes: ['username']
-        },
+
         model: Tag,
         attributes: ['id', 'tag_name']
       }
@@ -60,7 +57,7 @@ router.get('/:id', (req, res) => {
         res.status(404).json({ message: 'No product found with this id' });
         return;
       }
-      res.json(dbPostData);
+      res.json(dbProductData);
     })
     .catch(err => {
       console.log(err);
